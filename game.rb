2f06ff4a698f960d8f66de06b20fab7f08e4d5a5
ajhanson22227 <ajhanson22227@gmail.com>
2020-07-15 17:47:25 -0500

    Add player input method

--- game.rb
+++ game.rb
@@ -1,27 +1,27 @@
+require 'colorize'
+
 #Game engine class
 class Game
     attr_accessor :code
     def initialize
-        @code = Code.new
+        @secret_code = Code.new
+        @player = Player.new
     end
 
     def play
-        @code.generate_code
-        @code.display_code
+        @secret_code.generate_code
+        @secret_code.display_code
+        get_player_input
+        @secret_code.compare_code(@player.player_guess)
     end
 
-    
+    def get_player_input
+        until @player.player_guess.length == @secret_code.code_length
+            @player.player_input
+        end
+    end
+
 end
-
-# def new_code
-#   a = (0..9).to_a.shuffle
-#   new_code = []
-#   5.times do |i|
-#     new_code[i] = a.pop
-#   end
-#   new_code
-# end
-
 
 class Code
     def initialize
@@ -36,6 +36,36 @@ class Code
     end
 
     def display_code
+        puts @code.join('')
+    end
+
+    def compare_code(guess)
         p @code
+        p guess
+    end
+
+    def code_length
+        @code.length
+    end
+end
+
+class Player
+    attr_accessor :player_guess
+    def initialize
+        @name = 'You'
+        @player_guess = ''
+    end
+
+    def player_input
+        valid = false
+        until valid
+            puts 'What is your Guess? (Ex: 12345)'
+            @player_guess = gets.chomp
+            valid = check_valid_guess
+        end 
+    end
+
+    def check_valid_guess
+        @player_guess !~ /\D/
     end
 end
